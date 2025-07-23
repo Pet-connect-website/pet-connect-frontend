472147f59da9c2bac22f7607b24808b5f0ec672b
Dinithi1625403 <dinithi1625403@gmail.com>
2025-07-23 21:41:31 +0530

    made some changes

--- src/pages/Home/CommunitySection.jsx
+++ src/pages/Home/CommunitySection.jsx
@@ -60,7 +60,7 @@ export default function CommunitySection() {
                 boxShadow:
                   'rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px',
               }}
-              className="bg-gradient-to-r from-[#7c4a36] to-[#a67c52] text-white px-6 py-3 rounded-full font-bold text-lg transition-all duration-200 hover:bg-[#4b2f25] hover:scale-105 shadow-lg"
+              className="bg-gradient-to-r from-[#7c4a36] to-[#a67c52] text-white px-6 py-3 rounded font-bold text-lg transition-all duration-200 hover:bg-[#4b2f25] hover:scale-105 shadow-lg"
             >
               Visit Wall
             </button>
